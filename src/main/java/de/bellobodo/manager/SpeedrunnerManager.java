abb94669a70404124dbbf50101246ddbaa23f31b
BelloBodo <bellobodo@gmail.com>
2023-02-28 21:08:52 +0100

    Edited SpeedrunnerManager

---
+++ src/main/java/de/bellobodo/manager/SpeedrunnerManager.java
@@ -0,0 +1,41 @@
+package de.bellobodo.manager;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class SpeedrunnerManager {
+
+    private static ArrayList<Player> speedrunner = new ArrayList<>();
+
+    /**
+     @return true if Player is a Speedrunner
+     */
+    public static boolean isSpeedrunner(Player player) {
+        return speedrunner.contains(player);
+    }
+
+    /**
+    @return true if new Player was added
+     */
+    public static boolean addSpeedrunner(Player player) {
+        if (speedrunner.contains(player)) {
+            return false;
+        } else {
+            speedrunner.add(player);
+            return true;
+        }
+    }
+
+    /**
+     @return true if Player was removed
+     */
+    public static boolean removeSpeedrunner(Player player) {
+        if (speedrunner.contains(player)) {
+            speedrunner.remove(player);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
